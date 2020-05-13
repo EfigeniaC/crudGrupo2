29cfd00410358f587d888d1c5699c3f4763cb0ef
Cynthia Cajo <cynthia_30_11_09@hotmail.com>
2020-05-12 19:50:48 -0500

    Agregar archivos de proyecto.

---
+++ SistemaLectores/Conexion/ConexionMaestra.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Configuration
+
+Module ConexionMaestra
+    Public conexion As New SqlConnection("Server=DESKTOP-3GC35CO\EFI_SERVER;Database=Bd_CRUD ;User Id=sa ;Password=sa")
+
+    Sub Abrir_Conexion()
+        If conexion.State = 0 Then
+            conexion.Open()
+        End If
+    End Sub
+
+    Sub Cerrar_Conexion()
+        If conexion.State = 1 Then
+            conexion.Close()
+        End If
+    End Sub
+
+End Module
